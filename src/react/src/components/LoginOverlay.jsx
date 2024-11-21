65d211fcd56f98bad2ba2026aca2890100ef52d1
g-battaglia-h-farm <giacomo.battaglia@jakala.com>
2024-11-21 16:24:29 +0100

    Fix minori

--- src/react/src/components/LoginOverlay.jsx
+++ src/react/src/components/LoginOverlay.jsx
@@ -1,29 +1,26 @@
 // LoginOverya.jsx
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useAuthStore from '../zustand/auth';
 import API from '../API';
 import { query_parameter_with_default } from '../utils';
 
 const LoginOverlay = () => {
     const token = useAuthStore((state) => state.token);
-    const [link, setLink] = useState('');
 
     useEffect(() => {
         const code = query_parameter_with_default('code', '');
         const state = query_parameter_with_default('state', '');
-        const sessionState = query_parameter_with_default('session_state', '');
 
         if (!code) {
             console.log('Missing code, state or session_state');
             const checkLogin = async () => {
                 const response = await API.login(token);
                 if (response.success) {
-                    // Token is valid, proceed with the application
                     console.log('Login successful');
                 } else {
-                    // Token is invalid, redirect to the provided URL
                     console.log('Login failed, redirecting to', response.link);
                     window.location.href = response.link;
+                    return;
                 }
             };
 
